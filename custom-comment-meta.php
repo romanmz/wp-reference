342088f720b0c48408e0f0d49262091ca94501e3
Román Martínez <roman.mx3@gmail.com>
2019-02-14 15:21:41 -0600

    Split custom-fields.php into 2 files

---
+++ custom-comment-meta.php
@@ -0,0 +1,12 @@
+<?php
+/*
+==================================================
+CUSTOM COMMENT META
+==================================================
+https://www.smashingmagazine.com/2012/05/adding-custom-fields-in-wordpress-comment-form/
+
+COMMENT META
+get_comment_meta()
+add_comment_meta()
+update_comment_meta()
+delete_comment_meta()
--- custom-post-meta.php
+++ custom-post-meta.php
@@ -1,18 +1,12 @@
 <?php
 /*
 ==================================================
-CUSTOM FIELDS
+CUSTOM POST META
 ==================================================
 https://codex.wordpress.org/Custom_Fields
 
-FUNCTIONS
+POST META
 get_post_meta()
 add_post_meta()
 update_post_meta()
 delete_post_meta()
-
-METABOXES
-https://developer.wordpress.org/plugins/metadata/custom-meta-boxes/
-
-add_meta_box()
-remove_meta_box()

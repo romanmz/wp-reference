491312d6336012beb2d414a167204440215fa88e
Román Martínez <roman.mx3@gmail.com>
2019-02-15 14:26:16 -0600

    Added taxonomies folder

---
+++ taxonomies/post_format.php
@@ -0,0 +1,18 @@
+<?php
+/*
+==================================================
+FORMATS [pending]
+==================================================
+
+Format Archives
+
+Built-in Terms:
+	aside
+	gallery
+	link
+	image
+	quote
+	status
+	video
+	audio
+	chat

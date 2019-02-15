56cd3ce5e256ea5d993f45e7d3b5075a8d52b095
Román Martínez <roman.mx3@gmail.com>
2019-02-15 15:56:59 -0600

    Updated http-api.php

--- http-api.php
+++ http-api.php
@@ -4,6 +4,7 @@
 HTTP API
 ==================================================
 https://codex.wordpress.org/HTTP_API
+https://developer.wordpress.org/plugins/http-api/
 
 PERFORM REQUESTS
 wp_remote_get()                             // GET request
@@ -17,3 +18,9 @@ wp_remote_retrieve_header()                 // Retrieve a single HTTP header
 wp_remote_retrieve_headers()                // Retrieve all headers
 wp_remote_retrieve_response_code()          // Retrieve the response code
 wp_remote_retrieve_response_message()       // Retrieve the response message
+
+RESPONSE CODES
+2xx		Request was successful
+3xx		Request was redirected to another URL
+4xx		Request failed due to client error. Usually invalid authentication or missing data
+5xx		Request failed due to a server error. Commonly missing or misconfigured configuration files

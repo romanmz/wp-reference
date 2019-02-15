8400b319c76a1efb7c8c4462b5d5f2e155ce37f0
Román Martínez <roman.mx3@gmail.com>
2019-02-15 17:35:33 -0600

    Added cache.php

---
+++ cache.php
@@ -0,0 +1,7 @@
+<?php
+/*
+==================================================
+WP OBJECT CACHE [pending]
+==================================================
+https://codex.wordpress.org/Class_Reference/WP_Object_Cache
+https://codex.wordpress.org/WordPress_Optimization/Caching

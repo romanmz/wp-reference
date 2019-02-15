c40a65b3be8940fc8e00bc25f862ce3ba959f8d8
Román Martínez <roman.mx3@gmail.com>
2019-02-15 01:22:49 -0600

    Added filesystem-api.php

---
+++ filesystem-api.php
@@ -0,0 +1,6 @@
+<?php
+/*
+==================================================
+FILESYSTEM API (pending)
+==================================================
+https://codex.wordpress.org/Filesystem_API

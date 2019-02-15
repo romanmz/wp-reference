899eb48219ea1b5fd2e948f2875c4bc17c406393
Román Martínez <roman.mx3@gmail.com>
2019-02-15 17:37:42 -0600

    Added gutenberg-editor.php

---
+++ gutenberg-editor.php
@@ -0,0 +1,6 @@
+<?php
+/*
+==================================================
+GUTENBERG EDITOR [pending]
+==================================================
+https://wordpress.org/gutenberg/handbook/

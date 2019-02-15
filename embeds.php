d577d618f3dcf6def9c5e5d2a2f0b4e7a30160a6
Román Martínez <roman.mx3@gmail.com>
2019-02-15 15:44:48 -0600

    Added embeds.php

---
+++ embeds.php
@@ -0,0 +1,10 @@
+<?php
+/*
+==================================================
+EMBEDS [pending]
+==================================================
+https://codex.wordpress.org/Embeds
+
+ADD OEMBED PROVIDER
+
+REGISTER CUSTOM HANDLER FOR NON-OEMBED SITES

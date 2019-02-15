cd22a724167f3a07ca0b524530c5761de8022303
Román Martínez <roman.mx3@gmail.com>
2019-02-15 16:07:59 -0600

    Added metadata-api.php

---
+++ metadata-api.php
@@ -0,0 +1,13 @@
+<?php
+/*
+==================================================
+METADATA API
+==================================================
+https://codex.wordpress.org/Metadata_API
+https://developer.wordpress.org/plugins/metadata/
+
+HANDLING METADATA
+get_metadata()
+add_metadata()
+update_metadata()
+delete_metadata()

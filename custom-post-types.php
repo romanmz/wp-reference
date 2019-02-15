e7d2d10397be7f4664ed2669fb2e6e68f78c632b
Román Martínez <roman.mx3@gmail.com>
2019-02-14 20:30:11 -0600

    Updated labels on register_post_type

--- custom-post-types.php
+++ custom-post-types.php
@@ -35,30 +35,43 @@ function register_movie_post_type() {
 	$post_type      = 'movie'; // max 20 characters, all lowercase and no spaces
 	$singular_name  = 'Movie';
 	$plural_name    = 'Movies';
+	$singular_lc    = strtolower( $singular_name );
+	$plural_lc      = strtolower( $plural_name );
 	$url_slug       = 'movies';
 	
 	// Prepare labels
 	$labels = [
-		// Generic
-		'name'                => $plural_name,
-		'singular_name'       => $singular_name,
-		// Admin bar
-		'name_admin_bar'      => $singular_name,
-		// Admin menu
-		'menu_name'           => $plural_name,
-		'all_items'           => "All $plural_name",
-		'add_new'             => "Add New",
-		// Edit screen
-		'add_new_item'        => "Add New $singular_name",
-		'edit_item'           => "Edit $singular_name",
-		'view_item'           => "View $singular_name",
-		// Posts table
-		'search_items'        => "Search $plural_name",
-		'not_found'           => "No $plural_name found",
-		'not_found_in_trash'  => "No $plural_name found in Trash",
-		'parent_item_colon'   => "Parent $singular_name:",
-		// ???
-		'new_item'            => "New $singular_name",
+		'name'                     => $plural_name,
+		'singular_name'            => $singular_name,
+		'add_new'                  => "Add New",
+		'add_new_item'             => "Add New $singular_name",
+		'edit_item'                => "Edit $singular_name",
+		'new_item'                 => "New $singular_name",
+		'view_item'                => "View $singular_name",
+		'view_items'               => "View $plural_name",
+		'search_items'             => "Search $plural_name",
+		'not_found'                => "No $plural_lc found.",
+		'not_found_in_trash'       => "No $plural_lc found in Trash.",
+		'parent_item_colon'        => "Parent $singular_name:",
+		'all_items'                => "All $plural_name",
+		'archives'                 => "$singular_name Archives",
+		'attributes'               => "$singular_name Attributes",
+		'insert_into_item'         => "Insert into $singular_lc",
+		'uploaded_to_this_item'    => "Uploaded to this $singular_lc",
+		'featured_image'           => "Featured Image",
+		'set_featured_image'       => "Set featured image",
+		'remove_featured_image'    => "Remove featured image",
+		'use_featured_image'       => "Use as featured image",
+		'filter_items_list'        => "Filter $plural_lc list",
+		'items_list_navigation'    => "$plural_name list navigation",
+		'items_list'               => "$plural_name list",
+		'item_published'           => "$singular_name published.",
+		'item_published_privately' => "$singular_name published privately.",
+		'item_reverted_to_draft'   => "$singular_name reverted to draft.",
+		'item_scheduled'           => "$singular_name scheduled.",
+		'item_updated'             => "$singular_name updated.",
+		'name_admin_bar'           => $singular_name,
+		'menu_name'                => $plural_name,
 	];
 	
 	// Configure settings

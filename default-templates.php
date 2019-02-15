1b492e73688c21811cf18ac6623c3ceb56bd7bae
Román Martínez <roman.mx3@gmail.com>
2019-02-15 15:42:37 -0600

    Added default-templates.php

---
+++ default-templates.php
@@ -0,0 +1,48 @@
+<?php
+/*
+==================================================
+DEFAULT TEMPLATES [pending]
+==================================================
+https://developer.wordpress.org/themes/basics/template-hierarchy/
+
+
+// SITE SECTIONS
+// ------------------------------
+
+404
+404 page
+
+AUTHOR
+Author archives
+
+DATES
+Year archives
+Month archives
+Day archives
+
+SEARCH
+Search results
+
+
+// SITE CONTENT
+// ------------------------------
+
+SEARCH
+Search form
+
+COMMENTS
+Comments form
+Comment item
+Pingback item
+Trackback item
+
+EMBEDDED POSTS
+Embedded post type + format
+Embedded post type
+Embedded post (generic)
+
+PAGINATION
+Posts pagination (prev/next)
+Posts pagination (numbered)
+Comments pagination (prev/next)
+Content pagination (numbered)

72e57cb8be9d039d95ceebc5239a79bf7cc1aa2d
Román Martínez <roman.mx3@gmail.com>
2019-02-15 14:06:13 -0600

    Added post-types folder

---
+++ post-types/attachment.php
@@ -0,0 +1,8 @@
+<?php
+/*
+==================================================
+ATTACHMENTS [pending]
+==================================================
+
+Single Post
+Single Post (mime type/subtype/type_subtype)

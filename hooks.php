58db938deb660b3b1ea1a930ac5413c916fd8ed6
Román Martínez <roman.mx3@gmail.com>
2019-02-15 18:09:23 -0600

    Updated hooks.php

--- hooks.php
+++ hooks.php
@@ -5,6 +5,9 @@ HOOKS
 ==================================================
 https://codex.wordpress.org/Plugin_API/Action_Reference
 https://codex.wordpress.org/Plugin_API/Filter_Reference
+https://developer.wordpress.org/plugins/hooks/
+https://developer.wordpress.org/plugins/hooks/actions/
+https://developer.wordpress.org/plugins/hooks/filters/
 
 ACTIONS
 add_action()
